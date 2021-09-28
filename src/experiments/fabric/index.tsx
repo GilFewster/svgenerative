751c404a39d4a25be38cd385922cab90c3175a6e
Gil Fewster <gil.fewster@artprocessors.net>
2021-09-28 21:04:18 +1000

    spliner with fabricjs

--- src/experiments/fabric/index.tsx
+++ src/experiments/fabric/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { fabric } from "fabric";
 
@@ -7,16 +7,51 @@ import { useCanvasArtboard } from "../../hooks/artboards";
 
 import { ControlPanel } from "../../components/control-panel";
 import { PageArea } from "../../components/page-area";
+import {
+  IncrementingFunction,
+  useIncrementingLooper,
+} from "../../hooks/loopers";
+
+import { random, spline } from "@georgedoescode/generative-utils/src";
+
+const alphaIncrementor: IncrementingFunction = (current) =>
+  Math.round((current - 0.05) * 100) / 100;
+
+const colorIncrementor: IncrementingFunction = (current: number) =>
+  current + 10;
 
 export const Fabric = () => {
   const { Artboard, artboardSize, canvasId } = useCanvasArtboard();
   const [canvas, setCanvas] = useState<fabric.Canvas>();
 
+  const [numSteps] = useState(8);
+  const [yVariance] = useState(15);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const points: IPoint2D[] = [];
+  const requestRef = useRef<number>();
+  const lineAlpha = useIncrementingLooper({
+    startValue: 1,
+    minValue: 0,
+    incrementor: alphaIncrementor,
+  });
+  const lineColors = useIncrementingLooper({
+    startValue: 0,
+    maxValue: 180,
+    incrementor: colorIncrementor,
+  });
+
   useEffect(() => {
     if (canvasId && !canvas) setCanvas(new fabric.Canvas(canvasId));
   }, [canvasId, canvas]);
 
+  const stop = () => {
+    setIsAnimating(false);
+    requestRef.current && cancelAnimationFrame(requestRef.current);
+  };
+
   const clear = () => {
+    requestRef.current && cancelAnimationFrame(requestRef.current);
     canvas && canvas.clear();
   };
 
@@ -24,44 +59,50 @@ export const Fabric = () => {
     if (!canvas || !artboardSize) return;
     canvas.setWidth(artboardSize.width);
     canvas.setHeight(artboardSize.height);
-
-    // canvas.renderAll();
   }, [artboardSize, canvas]);
 
   const start = () => {
     if (!canvas) return;
 
-    const squareSize = 20;
+    if (isAnimating) return;
 
-    canvas.add(
-      new fabric.Rect({
-        width: squareSize,
-        height: squareSize,
-        left: 0,
-        top: artboardSize.height / 2 - squareSize / 2,
-        fill: "#0099s",
-      })
-    );
+    const { width, height } = artboardSize;
+    const yCenter = height / 2;
+    const stepSize = width / numSteps;
 
-    canvas.add(
-      new fabric.Rect({
-        width: squareSize,
-        height: squareSize,
-        left: artboardSize.width - squareSize,
-        top: 0,
-        fill: "#993300",
-      })
-    );
+    points.length = 0;
 
-    canvas.add(
-      new fabric.Rect({
-        width: squareSize,
-        height: squareSize,
-        left: artboardSize.width - squareSize,
-        top: artboardSize.height - squareSize,
-        fill: "#339900",
-      })
-    );
+    const yPosBase = yCenter + random(height * -0.25, height * 0.25);
+    for (let x = 0; x <= width; x += stepSize) {
+      points.push({ x, y: yPosBase });
+    }
+
+    setIsAnimating(true);
+    update();
+  };
+
+  const update = () => {
+    const color = lineColors.next().value;
+    const stroke = `rgba(${color},${color},${color},${lineAlpha.next().value})`;
+
+    points.forEach((point) => {
+      point.y += random(yVariance * -1, yVariance);
+    });
+
+    const pathData = spline(points, 1, false);
+    try {
+      canvas &&
+        canvas.add(
+          new fabric.Path(pathData, {
+            fill: "",
+            stroke: stroke,
+          })
+        );
+    } catch (e) {
+      console.log(e);
+    }
+
+    requestRef.current = requestAnimationFrame(update);
   };
 
   return (
@@ -69,7 +110,9 @@ export const Fabric = () => {
       <PageArea areaName={"artboard"}>{Artboard}</PageArea>
       <PageArea areaName={"controls"}>
         <ControlPanel>
-          <Button onClick={() => start()}>Draw</Button>
+          <Button onClick={() => (isAnimating ? stop() : start())}>
+            {isAnimating ? "Stop" : "Draw"}
+          </Button>
           <Button onClick={clear}>Clear</Button>
           <p>
             {artboardSize.width} x {artboardSize.height}
